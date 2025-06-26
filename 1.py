a6edaedac5ab5c4c8048cf51248dfb67d53d6f73
gursimran1004 <gursimrank1004@gmail.com>
2025-06-26 12:14:11 +0530

    edited

---
+++ 1.py
@@ -0,0 +1,12 @@
+num=11
+if num%2 ==0:
+    print ("number is even")
+else:
+    print("number is odd")
+
+# Age
+Age=20
+if  Age<18:
+    print("you are a cild")
+else:
+    print("you are a adult")
