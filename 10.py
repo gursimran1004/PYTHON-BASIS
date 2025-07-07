c3dc85f566b04fa4083e998b7c612b11aaa83fab
gursimran1004 <gursimrank1004@gmail.com>
2025-07-07 14:04:29 +0530

    mini project

--- 10.py
+++ 10.py
@@ -1,1 +1,43 @@
 import sqlite3
+conn = sqlite3.connect("example.db")
+cur = conn.cursor()
+while True:
+    print("\nMenu:")
+    print("1. Create Student Table")
+    print("2. Delete Student Table")
+    print("3. Retrieve Student Data")
+    print("4. Exit")
+    print("5. Insert Student Data")
+    
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        cur.execute("CREATE TABLE IF NOT EXISTS student (roll INTEGER, name TEXT, marks INTEGER)")
+        conn.commit()
+        print("Student table created.")
+
+    elif choice == '2':
+        cur.execute("DROP TABLE IF EXISTS student")
+        conn.commit()
+        print("Student table deleted.")
+
+    elif choice == '3':
+
+        cur.execute("SELECT * FROM student")
+        table_exists = cur.fetchall()
+
+        if table_exists:
+            cur.execute("SELECT * FROM student")
+            data = cur.fetchall()
+            if data:
+                print("Student Records:")
+                for row in data:
+                    print(row)
+            else:
+                print("Student table is empty.")
+        else:
+            print("Student table does not exist.")
+
+    elif choice == '4':
+        print("Exiting...")
+conn.close()
---
+++ 11.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root=tk.Tk()
+root.title("TIC TAC TOE GAME")
+
+
+board = [" " for _ in range(9)]
+
+def print_board():
+    print()
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print("--+---+--")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print("--+---+--")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print()
+
+def winner(p):
+    win = [(0,1,2),(3,4,5),(6,7,8), (0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+    for a,b,c in win:
+        if board[a] == board[b] == board[c] == p:
+            return True
+    return False
+
+def play_game():
+    current = "X"
+    for turn in range(9):
+        print_board()
+        move = int(input(f"Player {current}, enter position (1-9): ")) - 1
+
+        if move < 0 or move > 8:
+            print("Invalid position! Try again.")
+            continue
+
+        if board[move] != " ":
+            print("Already taken! Try again.")
+            continue
+
+        board[move] = current
+
+        if winner(current):
+            print_board()
+            print(f"🎉 Player {current} is the winner!")
+            return
+
+        current = "O" if current == "X" else "X"
+
+    print_board()
+    print("It's a draw!")
+
+# Start the game
+play_game()
